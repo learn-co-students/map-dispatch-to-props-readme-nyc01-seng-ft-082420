bac93fc0f77e4f3ff11dcad4a0db6a03c1b3b952
jdhofmann <justindhofmann@gmail.com>
2020-11-19 10:39:00 -0500

    ha

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
 import { createStore } from 'redux';
 import shoppingListItemReducer from './reducers/shoppingListItemReducer';
-import App from './App';
-import './index.css';
 import { Provider } from 'react-redux';
 
 const store = createStore(shoppingListItemReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
